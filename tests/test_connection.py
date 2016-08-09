4a7d8f2906bcfbc8ece49816ba317220e3cc0f76
Phil H <phil.hildebrand@moz.com>
2016-08-09 12:06:24 -0700

    Added connection test and after script unit test

---
+++ tests/test_connection.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pymongo as m
+
+#############################################################################
+#    Connection details
+#############################################################################
+
+try:
+    conn = m.MongoClient('mongodb://localhost:27017/'
+    info = conn.server_info()
+    print(' Connected : %s' % info)
+
+except Exception as e:
+    print('Fatal: Could not connect to mongo database')
+    sys.exit(2)
+
