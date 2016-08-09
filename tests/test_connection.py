d0c7d364fc848f0250157bd8afa631684917c63b
Phil H <phil.hildebrand@moz.com>
2016-08-09 13:53:08 -0700

    Fixed connection test

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pymongo as m
+from unittest import TestCase
 
-def test_connection():
+def connect():
 
-    #############################################################################
-    #    Connection details
-    #############################################################################
-    
     try:
-        conn = m.MongoClient('mongodb://localhost:27017/')
+        # conn = m.MongoClient('mongodb://localhost:27017/?connectTimeoutMS=2000&socketTimeoutMS=2000')
+        conn = m.MongoClient('localhost',27017,connectTimeoutMS=2000,socketTimeoutMS=2000)
         info = conn.server_info()
-        print(' Connected : %s' % info)
+        return True
     
     except Exception as e:
-        print('Fatal: Could not connect to mongo database')
-        sys.exit(2)
+        return False
 
+class test_connection(TestCase):
+    def test(self):
+        self.assertTrue(connect())
