6beb1f77a7dfba05be53f4547535d0510b0fc77b
Phil H <phil.hildebrand@moz.com>
2016-08-09 18:48:31 -0700

    fixed db test

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -9,17 +9,12 @@ class test_connection(t.TestCase):
         '''We should be able to create a connection'''
 
         self.conn = m.MongoClient('localhost',27017,connectTimeoutMS=2000,socketTimeoutMS=2000)
-        self.db = ""
-
-    def test_create(self):
-        '''We should be able to create a database'''
-
         self.db = self.conn['mongo_test']
-   
+
     def test_get_count(self):
         '''We should be able to create a collection'''
 
-        self.assertGreater(self.db['mongo_collection'].count(),-1)
+        counter = self.db['mongo_collection'].count()
 
 # if __name__ == '__main__':
 #    t.main()
