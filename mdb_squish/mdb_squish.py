41291867755b9e87248a382ea795351d52f0a857
Phil H <phil.hildebrand@moz.com>
2016-08-16 17:16:05 -0700

    change stats-rate to rate

--- mdb_squish/mdb_squish.py
+++ mdb_squish/mdb_squish.py
@@ -65,7 +65,7 @@ parser.add_argument('--collections', nargs='+',
                     help='limit compaction to these collections')
 parser.add_argument('-c', '--concurrency', default=3, type=int,
                     help='# of compaction threads (default=3)')
-parser.add_argument('-r', '--stats-rate', default=2, type=int,
+parser.add_argument('-r', '--rate', default=2, type=int,
                     help='update stats file after x compcations (default=2)')
 parser.add_argument('--log-dir', default='/var/log',
                     help='MongoDB Get compaction log file location (default=/var/log)')
@@ -199,7 +199,7 @@ for (compact_db, collection, stats, diff, duration) in pool.imap(compact, compac
     collection_stats[collection]['compacted'] = 1
     log.debug('%s.%s stats: \n%s (%d, %0.4f)\n' % (compact_db, collection, stats, diff, duration))
     log.debug('\n  %s \n' % (json.dumps(collection_stats)))
-    if (total_collections % stats_rate) == 0:
+    if (total_collections % args.rate) == 0:
         with open('%s/mdb_squish_%s_stats.json' % (stats_dir, compact_db, collection), 'w') as outfile:
             json.dump(collection_stats, outfile)
 
